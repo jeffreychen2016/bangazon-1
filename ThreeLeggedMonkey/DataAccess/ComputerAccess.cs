487e35b30bd8327a349ff45ea344d7fe9a1b46ed
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-11-13 17:45:41 -0600

    added linq to GetAllComputers method to output only computer name

--- ThreeLeggedMonkey/DataAccess/ComputerAccess.cs
+++ ThreeLeggedMonkey/DataAccess/ComputerAccess.cs
@@ -18,21 +18,32 @@ namespace ThreeLeggedMonkey.DataAccess
             ConnectionString = config.GetSection("ConnectionString").Value;
         }
 
-        public List<Computer> GetAllComputers()
+        public List<string> GetAllComputers()
         {
             using (var dbConnection = new SqlConnection(ConnectionString))
             {
                 dbConnection.Open();
 
-                var result = dbConnection.Query<Computer>(@"SELECT 
+                var computers = dbConnection.Query<Computer>(@"SELECT 
                                                             Id
                                                             ,SerialNumber
                                                             ,DateOfPurchase
                                                             ,DecommissionedDate
                                                             ,IsOperable
                                                           FROM computers");
+                var result = from c in computers
+                             select c.SerialNumber;
+
                 return result.ToList();
             }
         }
+
+        //public Computer GetComputerPerId(int id)
+        //{
+        //    using (var dbConnection = new SqlConnection(ConnectionString))
+        //    {
+        //        dbConnection.Open()
+        //    }
+        //}
     }
 }
