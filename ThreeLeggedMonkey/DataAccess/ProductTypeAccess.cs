a618912fc322e5496fdf09c7883972a5cf89f75f
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-11-12 19:39:18 -0600

    set up connection for ProductType

---
+++ ThreeLeggedMonkey/DataAccess/ProductTypeAccess.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThreeLeggedMonkey.DataAccess
+{
+    public class ProductTypeAccess
+    {
+        private string ConnectionString;
+
+        public ProductTypeAccess(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+    }
+}
