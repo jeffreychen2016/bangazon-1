fb50fff5e797ccc84ab45de0f42a8006fcf94653
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-11-13 17:08:06 -0600

    set up connection for computer controller

---
+++ ThreeLeggedMonkey/DataAccess/ComputerAccess.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThreeLeggedMonkey.DataAccess
+{
+    public class ComputerAccess
+    {
+        public ComputerAccess(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+    }
+}
