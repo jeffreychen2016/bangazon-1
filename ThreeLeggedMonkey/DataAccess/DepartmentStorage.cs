03a5ab7d87edc92979ed8df5593c700b801e4936
Leo Taylor <leotaylor723@gmail.com>
2018-11-13 20:44:29 -0600

    get all, get by id, post Departments

---
+++ ThreeLeggedMonkey/DataAccess/DepartmentStorage.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ThreeLeggedMonkey.Models;
+
+namespace ThreeLeggedMonkey.DataAccess
+{
+    public class DepartmentStorage
+    {
+        private readonly string ConnectionString;
+
+        public DepartmentStorage(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        public Department GetById(int id)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                db.Open();
+
+                var result = db.QueryFirst<Department>(@"SELECT *
+                                                    FROM Department
+                                                    WHERE Id = @id", new { id });
+                return result;
+            }
+        }
+
+        public List<Department> GetAll()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                db.Open();
+
+                var result = db.Query<Department>(@"SELECT * FROM Department");
+
+                return result.ToList();
+            }
+        }
+
+        public bool Add(Department department)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                db.Open();
+
+                var result = db.Execute(@"INSERT INTO [dbo].[Department]([DepartmentName])
+                                            VALUES(@DepartmentName)", department);
+
+                return result == 1;
+            }
+        }
+    }
+}
