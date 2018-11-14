c0166e4692a7d3442653f35390480350d45cf88a
Tom Dugan <thomas@aadwebb.com>
2018-11-13 19:09:45 -0600

    returns all customers

---
+++ ThreeLeggedMonkey/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ThreeLeggedMonkey.DataAccess;
+
+namespace ThreeLeggedMonkey.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // passes configuration thru to storage
+        private IConfiguration _config;
+
+        public CustomerController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private readonly CustomerStorage _storage;
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var customers = new CustomerStorage(_config);
+            return Ok(customers.GetAllCustomers());
+        }
+    }
+}
