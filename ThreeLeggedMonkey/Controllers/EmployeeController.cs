93ecf9d3370e0d561420d43ac755dcf5bfa2bd7d
Leo Taylor <leotaylor723@gmail.com>
2018-11-13 18:23:06 -0600

    get single employee, join with department and computer

---
+++ ThreeLeggedMonkey/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ThreeLeggedMonkey.DataAccess;
+
+namespace ThreeLeggedMonkey.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeStorage _storage;
+
+        public EmployeeController(IConfiguration config)
+        {
+            _storage = new EmployeeStorage(config);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            return Ok(_storage.GetById(id));
+        }
+    }
+}
---
+++ ThreeLeggedMonkey/DataAccess/EmployeeStorage.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using ThreeLeggedMonkey.Models;
+
+namespace ThreeLeggedMonkey.DataAccess
+{
+    public class EmployeeStorage
+    {
+        private readonly string ConnectionString;
+
+        public EmployeeStorage(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        public Employee GetById(int id)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.QueryFirst<Employee>(@"SELECT 
+	                                                            FullName = FirstName + ' ' + LastName,
+	                                                            Department = D.DepartmentName,
+	                                                            Computer = C.SerialNumber
+                                                            FROM Employee E
+                                                            JOIN Department D
+                                                            ON E.DepartmentId = D.Id
+                                                            JOIN Computers C
+                                                            ON E.AssignedComputer = C.Id
+                                                            WHERE E.Id = 5", new { id });
+                return result;
+            }
+        }
+    }
+}
