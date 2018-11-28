063dd7e0de58169394228d0019dd7799a31df3f3
Mary Alice Orr <m.a.orr17@gmail.com>
2018-11-27 21:07:08 -0600

    wip commit

---
+++ TwoLeggedMonkey/ClientApp/src/DBRequests/PaymentTypeCalls.js
@@ -0,0 +1,24 @@
+﻿import axios from 'axios';
+import { PaymentType } from '../components/PaymentType';
+
+const getAllPayments = () => {
+    return new Promise((resoslve, reject) => {
+        axios
+            .get(`/api/PaymentType`)
+            .then(res => {
+                const allPaymentTypes = [];
+                if (res.data !== null) {
+                    Object.keys(res.data).forEach(ptKey => {
+                        res.data[ptKey].id = ptKey;
+                        allPaymentTypes.push(res.data[ptKey]);
+                    });
+                }
+                resolve(allPaymentTypes);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+export default {getPay}
--- TwoLeggedMonkey/ClientApp/src/components/PaymentType.js
+++ TwoLeggedMonkey/ClientApp/src/components/PaymentType.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import paymentTypeCalls from '../../DBRequests/paymentTypeCalls';
+
 
 export class PaymentType extends Component {
+
+    state = {
+        allPaymentTypes: [],
+    }
+
+    componentDidMount() {
+        paymentTypeCalls
+            .then((allPaymentTypes) => {
+                this.setState({allPaymentTypes})
+            })
+            .catch((error) => {
+                console.error('error with allPayementTypes Get Call', error);
+            });
+    };
+
+
+
+
   render() {
     return (
-      <div>
-        <h1>PaymentType</h1>
+        <div>
+            <Link to={`/paymenttype`}>
+                <h1>PaymentType</h1>
+                <h3>{allPaymentTypes.Id}</h3>
+                <h3>{allPaymentTypes.PaymentType}</h3>
+               
+            </Link>
       </div>
     );
   }
