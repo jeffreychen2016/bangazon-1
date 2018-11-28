3aae7fc118cf847c076e8293fd59c127829687d7
Leo Taylor <leotaylor723@gmail.com>
2018-11-27 21:28:46 -0600

    prints product names

---
+++ TwoLeggedMonkey/ClientApp/src/DBRequests/productRequest.js
@@ -0,0 +1,24 @@
+﻿import axios from 'axios';
+
+const getRequest = () => {
+    return new Promise((resolve, reject) => {
+        axios
+            .get(`/api/product/products`)
+            .then(results => {
+                const products = [];
+                if (results.data !== null) {
+                    Object.keys(results.data).forEach(pkey => {
+                        results.data[pkey].id = pkey;
+                        products.push(results.data[pkey]);
+                    });
+                }
+                resolve(products);
+            })
+            .catch(error => {
+                reject(error);
+            });
+    });
+};
+
+
+export default { getRequest };
