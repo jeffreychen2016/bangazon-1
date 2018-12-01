48085f2a797299ff08a1e710379d8e578724de5f
Mary Alice Orr <m.a.orr17@gmail.com>
2018-12-01 11:42:30 -0600

    delete and get are now working properly

--- TwoLeggedMonkey/ClientApp/src/DBRequests/PaymentTypeCalls.js
+++ TwoLeggedMonkey/ClientApp/src/DBRequests/PaymentTypeCalls.js
@@ -4,20 +4,41 @@ const getAllPaymentTypes = () => {
     return new Promise((resolve, reject) => {
         axios
             .get(`/api/paymenttype`)
-            .then(res => {
-                const allPaymentTypes = [];
-                if (res.data !== null) {
-                    Object.keys(res.data).forEach(ptKey => {
-                        res.data[ptKey].id = ptKey;
-                        allPaymentTypes.push(res.data[ptKey]);
-                    });
-                }
+            .then(res =>
+            {
                 resolve(res.data);
             })
-            .catch(error => {
+            .catch(error =>
+            {
                 reject(error);
             });
     });
 };
 
-export default { getAllPaymentTypes }
+const postPaymentType = (paymentType) => {
+    return new Promise((resolve, reject) => {
+        axios
+            .post(`/api/paymenttype/`, paymentType)
+            .then((res) => {
+                resolve(res.data);
+            })
+            .catch((error) => {
+                reject(error);
+            })
+        })  
+    };
+
+const deletePaymentType = (id) => {
+    return new Promise((resolve, reject) => {
+        axios
+            .delete(`/api/paymenttype/delete/${id}`)
+            .then((res) => {
+                resolve(res.data);
+            })
+            .catch((err) => {
+                reject(err);
+            })
+    })
+};
+
+export default { getAllPaymentTypes, deletePaymentType, postPaymentType }
