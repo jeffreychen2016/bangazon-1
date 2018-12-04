99078719fa0e4870fa211891f4450f24d69e3a57
Mary Alice Orr <m.a.orr17@gmail.com>
2018-12-03 21:34:42 -0600

    wip commit

---
+++ TwoLeggedMonkey/ClientApp/src/components/PaymentTypeDelete.js
@@ -0,0 +1,33 @@
+﻿import React, { Component } from 'react';
+import { Glyphicon } from 'react-bootstrap';
+
+import paymentTypeCalls from '../DBRequests/PaymentTypeCalls';
+
+export class PaymentTypeDelete extends Component {
+
+    deletePaymentTypeEvent = (e) => {
+        paymentTypeCalls
+            .deletePaymentType(this.props.paymentTypeId)
+            .then(() => {
+                this.props.paymentTypeState();
+            })
+            .catch((error) => {
+                console.error('problem with delete Payment Type', error);
+            });
+    };
+
+    render() {
+            return (
+                <div>
+                    <button
+                        className="btn btn-danger"
+                        onClick={this.deletePaymentTypeEvent}>
+                        <Glyphicon glyph="trash"/>
+                    </button>
+                </div>
+            );
+
+    }
+}
+
+
