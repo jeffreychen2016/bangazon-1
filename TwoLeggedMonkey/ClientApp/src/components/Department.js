078f8335dfa5d95d005264fc7f7809fd8332d033
Leo Taylor <leotaylor723@gmail.com>
2018-12-10 20:34:25 -0600

    wip

--- TwoLeggedMonkey/ClientApp/src/components/Department.js
+++ TwoLeggedMonkey/ClientApp/src/components/Department.js
@@ -1,95 +1,83 @@
 import React, { Component } from 'react';
 import departmentRequest from '../DBRequests/department';
 //import { DepartmentGrid } from './DepartmentGrid';
-import { Panel, Button } from 'react-bootstrap';
+//import { Panel, Button } from 'react-bootstrap';
 
 export class Department extends Component {
-  state = {
-      departments: [],
-      employees: [],
-      open: true
+    state = {
+        departments: [],
+        employees: [],
+        open: true
     };
 
-    componentDidMount() {
+    componentDidMount = () => {
         departmentRequest.getAllDepartments()
             .then((departments) => {
                 this.setState({ departments });
             })
             .catch((err) => {
-                console.error('Error getting all department: ', err);
+                console.error('Error adding an deparments: ', err);
             });
-    }
-
-      gettingEmployees() {
-          departmentRequest.getDeptEmployees()
-              .then((employees) => {
-                  this.setState({ employees });
-              })
-              .catch((err) => {
-                  console.error('Error getting employees', err);
-              });
-    }
-
-     //async componentDidMount() {
-    //    const deptRequest = departmentRequest.getAllDepartments();
-    //    const employeeRequest = departmentRequest.getDeptEmployees();
-    //    const data = await Promise.all([deptRequest, employeeRequest]).catch(error => console.error({ error }));
-    //    const departments = data[0].data;
-    //    const employees = data[1].data;
-    //    this.setState({ departments, employees });
-    //}
-
-     printGrid = () => {
-        const allDepartments = this.state.departments;
-        return allDepartments.map((department) => {
-            return (
-                <div>
-                    <div>{department.departmentName}</div>
-                    <Button onClick={() => this.setState({ open: !this.state.open })}> See Employee</Button>
-                    <br />
-                    <Panel expanded={this.state.open}>
-                        <Panel.Collapse>
-                            <Panel.Body>
-                                {this.printEmployees}
-                            </Panel.Body>
-                        </Panel.Collapse>
-                    </Panel>
-                </div>
-            );
-        });
     };
 
-    printEmployees = () => {
-        const employees = this.state.employees;
-        return employees.map((employee) => {
-            return (
-                <div>
-                    <div className="container">
-                        <table className="table">
-                            <tbody>
-                                <tr>
-                                    <th>Employee</th>
-                                </tr>
-                                <tr>
-                                    <td>{employee.firstName}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            );
-        });
-    };
+    //gettingEmployees() {
+    //    departmentRequest.getDeptEmployees()
+    //        .then((employees) => {
+    //            this.setState({ employees });
+    //        })
+    //        .catch((err) => {
+    //            console.error('Error getting employees', err);
+    //        });
+    // }
+
+
+
+
+    //printEmployees = () => {
+    //    const employees = this.state.employees;
+    //    return employees.map((employee) => {
+    //        return (
+    //            <div>
+    //                <div className="container">
+    //                    <table className="table">
+    //                        <tbody>
+    //                            <tr>
+    //                                <th>Employee</th>
+    //                            </tr>
+    //                            <tr>
+    //                                <td>{employee.firstName}</td>
+    //                            </tr>
+    //                        </tbody>
+    //                    </table>
+    //                </div>
+    //            </div>
+    //        );
+    //    });
+    //};
 
     render() {
-
-    return (
-      <div>
-            <h1>Department</h1>
-            <this.printGrid/>
-      </div>
-    );
-  }
+        const printGrid = this.state.departments.map((department) => {
+            return (
+                <tr key={department.id}>
+                    <td>{department.departmentName}</td>
+                    <td><button>See Employees</button></td>
+                </tr>
+            );
+        });
+        return (
+            <div>
+                <h1>Departments</h1>
+                <div className="container">
+                    <table className="table">
+                        <tbody>
+                            <tr>
+                                <th>Department</th>
+                            </tr>
+                                {printGrid}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default Department;
