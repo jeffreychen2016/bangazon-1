22f743cf210f7971d3a484b851f175de5f4ed240
Leo Taylor <leotaylor723@gmail.com>
2018-12-10 18:01:43 -0600

    wip

--- TwoLeggedMonkey/ClientApp/src/components/Department.js
+++ TwoLeggedMonkey/ClientApp/src/components/Department.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import departmentRequest from '../DBRequests/department';
-import { DepartmentGrid } from './DepartmentGrid';
+//import { DepartmentGrid } from './DepartmentGrid';
+import { Panel, Button } from 'react-bootstrap';
 
 export class Department extends Component {
   state = {
       departments: [],
-      employees: []
+      employees: [],
+      open: true
     };
 
-    //async componentDidMount() {
+    componentDidMount() {
+        departmentRequest.getAllDepartments()
+            .then((departments) => {
+                this.setState({ departments });
+            })
+            .catch((err) => {
+                console.error('Error getting all department: ', err);
+            });
+    }
+
+      gettingEmployees() {
+          departmentRequest.getDeptEmployees()
+              .then((employees) => {
+                  this.setState({ employees });
+              })
+              .catch((err) => {
+                  console.error('Error getting employees', err);
+              });
+    }
+
+     //async componentDidMount() {
     //    const deptRequest = departmentRequest.getAllDepartments();
     //    const employeeRequest = departmentRequest.getDeptEmployees();
     //    const data = await Promise.all([deptRequest, employeeRequest]).catch(error => console.error({ error }));
@@ -17,31 +39,57 @@ export class Department extends Component {
     //    this.setState({ departments, employees });
     //}
 
-  componentDidMount () {
-    departmentRequest.getAllDepartments()
-      .then((departments) => {
-        this.setState({departments});
-      })
-      .catch((err) => {
-        console.error('Error getting all department: ', err);
-          });
-      //departmentRequest.getDeptEmployees()
-      //    .then((employees) => {
-      //        this.setState({ employees });
-      //    })
-      //    .catch((err) => {
-      //        console.error('Error getting employees', err);
-      //    });
-  }
+     printGrid = () => {
+        const allDepartments = this.state.departments;
+        return allDepartments.map((department) => {
+            return (
+                <div>
+                    <div>{department.departmentName}</div>
+                    <Button onClick={() => this.setState({ open: !this.state.open })}> See Employee</Button>
+                    <br />
+                    <Panel expanded={this.state.open}>
+                        <Panel.Collapse>
+                            <Panel.Body>
+                                {this.printEmployees}
+                            </Panel.Body>
+                        </Panel.Collapse>
+                    </Panel>
+                </div>
+            );
+        });
+    };
 
-  render() {
+    printEmployees = () => {
+        const employees = this.state.employees;
+        return employees.map((employee) => {
+            return (
+                <div>
+                    <div className="container">
+                        <table className="table">
+                            <tbody>
+                                <tr>
+                                    <th>Employee</th>
+                                </tr>
+                                <tr>
+                                    <td>{employee.firstName}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            );
+        });
+    };
+
+    render() {
+
     return (
       <div>
-        <h1>Department</h1>
-        <DepartmentGrid 
-            departments = {this.state.departments}
-        />
+            <h1>Department</h1>
+            <this.printGrid/>
       </div>
     );
   }
 }
+
+export default Department;
--- TwoLeggedMonkey/ClientApp/src/components/DepartmentGrid.js
+++ TwoLeggedMonkey/ClientApp/src/components/DepartmentGrid.js
@@ -1,34 +1,53 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
+//import { Panel, Button } from 'react-bootstrap';
 
-export class DepartmentGrid extends Component {
+//export class DepartmentGrid extends Component {
 
-    componentDidMount = () => {
-     
-    }
+//    printGrid = () => {
+//        const allDepartments = this.props.departments;
+//        return allDepartments.map((department) => {
+//            return (
+//                <div>
+//                    <div>{department.departmentName}</div>
+//                    <Button onclick={() => this.setState({ open: !this.props.open })}> See Employee</Button>
+//                    <br />
+//                    <Panel expanded={this.props.open}>
+//                        <Panel.Collapse>
+//                            <Panel.Body>
+//                                {this.printEmployees}
+//                            </Panel.Body>
+//                        </Panel.Collapse>
+//                    </Panel>
+//                </div>
+//            );
+//        });
+//    }
 
-  printGrid = () => {
-    const allDepartments = this.props.departments;
-    return allDepartments.map((department) => {
-      return (
-        <tr key={department.id}>
-              <td>{department.departmentName}</td>
-        </tr>
-      );
-    });
-  };
+//    printEmployees = () => {
+//        const employees = this.props.employees;
+//        return employees.map((employee) => {
+//            return (
+//                <div>
+//                    <div className="container">
+//                        <table className="table">
+//                            <tbody>
+//                                <tr>
+//                                    <th>Employee</th>
+//                                </tr>
+//                                <tr>
+//                                    <td>{employee.firstName}</td>
+//                                </tr>
+//                            </tbody>
+//                        </table>
+//                    </div>
+//                </div>
+//            );
+//        });
+//    };
 
-  render() {
-    return (
-      <div className="container">
-        <table className="table">
-          <tbody>
-            <tr>
-              <th>Department Name</th>
-            </tr>
-            {this.printGrid()}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+//    render() {
+//        return (
+//            <this.printGrid />
+//        );
+//    }
+//}
