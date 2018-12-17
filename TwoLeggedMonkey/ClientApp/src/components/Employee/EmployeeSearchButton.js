cb7dc1e7e5aad3f4b8f2381bc76cfee5977fdc8b
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-12-16 18:05:37 -0600

    fixed bug there when drop-down set back to choose here option. it gets 400 error

--- TwoLeggedMonkey/ClientApp/src/components/Employee/EmployeeSearchButton.js
+++ TwoLeggedMonkey/ClientApp/src/components/Employee/EmployeeSearchButton.js
@@ -6,11 +6,11 @@ import employeeRequest from '../../DBRequests/employee';
 export class EmployeeSearchButton extends Component {
 
   getFilteredEmployees = () => {
-    const firstName = this.props.firstName === '' ? null : this.props.firstName;
-    const lastName = this.props.lastName === '' ? null : this.props.lastName;
-    const departmentId = this.props.department === '' ? null : this.props.department;
-    const employeeTypeId = this.props.employeeType === '' ? null : this.props.employeeType;
-    const assignedComputer = this.props.assignedComputer === '' ? null : this.props.assignedComputer;
+    const firstName = this.props.firstName === '' ||  this.props.firstName ===  'Choose here' ? null : this.props.firstName;
+    const lastName = this.props.lastName === '' || this.props.lastName ===  'Choose here' ? null : this.props.lastName;
+    const departmentId = this.props.department === '' || this.props.department ===  'Choose here' ? null : this.props.department;
+    const employeeTypeId = this.props.employeeType === '' || this.props.employeeType ===  'Choose here'  ? null : this.props.employeeType;
+    const assignedComputer = this.props.assignedComputer === '' || this.props.assignedComputer ===  'Choose here' ? null : this.props.assignedComputer;
 
     employeeRequest.getFilteredEmployees(firstName,lastName,departmentId,employeeTypeId,assignedComputer)
       .then((employees) => {
